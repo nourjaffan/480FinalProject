4b35fff8672cfc9e91eca9e17adedc6b002cf7c2
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-11-29 20:53:06 -0700

    Update accountDatabase.sql
    Fixed to work with MySQL

--- accountDatabase.sql
+++ accountDatabase.sql
@@ -1,36 +1,37 @@
-CREATE DATABASE IF NOT EXISTS 'db';
-USE 'db';
+DROP DATABASE IF EXISTS db;
+CREATE DATABASE db; 
+USE db;
 
-DROP TABLE IF EXISTS 'account';
-CREATE TABLE 'account'(
-    'Name'                  varchar(50),
-    'Address'               varchar(50),
-    'CardInfo'              int,
-    'BillingName'           varchar(50),
-    'BillingAddress'        varchar(50),
-    'Email'                 varchar(50),
-    'Password'              varchar(50),
-    'PhoneNumber'           int,
-    'BillingPhoneNumber'    int,
-    PRIMARY KEY ('Email')
-)
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(
+    Name                  varchar(45),
+    Address               varchar(45),
+    CardInfo              int,
+    BillingName           varchar(45),
+    BillingAddress        varchar(45),
+    Email                 varchar(45),
+    Password              varchar(45),
+    PhoneNumber           int,
+    BillingPhoneNumber    int,
+    PRIMARY KEY (Email)
+);
 
 
-DROP TABLE IF EXISTS 'ticket';
-CREATE TABLE 'ticket'
+DROP TABLE IF EXISTS ticket;
+CREATE TABLE ticket
     (
-    'SeatNumber'            int,
-    'Title'                 varchar(50),
-    'Cost'                  float,
-    'Date'                  varchar(50),
-    'UniqueTicket'          int AUTOINCREMENT,
-    PRIMARY KEY ('UniqueTicket')
-    )
+    SeatNumber            int,
+    Title                 varchar(50),
+    Cost                  float,
+    Date                  varchar(50),
+    UniqueTicket          int not null AUTO_INCREMENT,
+    PRIMARY KEY (UniqueTicket)
+    );
 
 
-DROP TABLE IF EXISTS 'reciept';
-CREATE TABLE 'reciept'
+DROP TABLE IF EXISTS reciept;
+CREATE TABLE reciept
     (
-    'UniqueNumber'          int,
-    PRIMARY KEY('UniqueNumber')
-    )
+    UniqueNumber          int,
+    PRIMARY KEY(UniqueNumber)
+    );
