36ea4a0b7d5086edef9a18932d1188ecf48d8bae
nourjaffan <73398943+nourjaffan@users.noreply.github.com>
2022-11-30 19:04:07 -0700

    Update accountDatabase.sql

--- accountDatabase.sql
+++ accountDatabase.sql
@@ -4,13 +4,14 @@ USE db;
 
 DROP TABLE IF EXISTS account;
 CREATE TABLE account(
-    Name                  varchar(45),
-    Address               varchar(45),
-    CardInfo              int,
-    BillingName           varchar(45),
-    BillingAddress        varchar(45),
-    Email                 varchar(45),
-    Password              varchar(45),
+    Name                  varchar(50),
+    Address               varchar(50),
+    CardNumber            int,
+    cardName              varchar(50);
+    BillingName           varchar(50),
+    BillingAddress        varchar(50),
+    Email                 varchar(50),
+    Password              varchar(50),
     PhoneNumber           int,
     BillingPhoneNumber    int,
     PRIMARY KEY (Email)
@@ -21,7 +22,7 @@ DROP TABLE IF EXISTS ticket;
 CREATE TABLE ticket
     (
     SeatNumber            int,
-    ShowTime              varchar(45),
+    ShowTime              varchar(50),
     Title                 varchar(50),
     Cost                  float,
     Date                  varchar(50),
