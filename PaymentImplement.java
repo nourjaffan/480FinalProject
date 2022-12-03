9655ace50e4298fa62dd0032c4e46d6d1e19d571
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-02 23:05:55 -0700

    Delete PaymentImplement.java

--- PaymentImplement.java
+++
@@ -1,9 +0,0 @@
-abstract class PaymentImplement {
-    protected PaymentStrategy paymentStrategy;
-    public void setPaymentStrategy(PaymentStrategy payment){
-        paymentStrategy = payment;
-    }
-    public void performPayment(){
-        paymentStrategy.pay();
-    }
-}
