00f628031065cfae8bad6c96ba84aebee7ad8711
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-04 15:26:18 -0700

    Update RegularSearchGUI.java

--- GUI/RegularSearchGUI.java
+++ GUI/RegularSearchGUI.java
@@ -13,7 +13,6 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
     private JLabel searchLabel;
     private JTextField searchField;
     private JButton searchButton;
-    private JButton backButton;
     private JLabel failSearch;
     private Container container;
     private Account acc;
@@ -24,7 +23,6 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         searchField = new JTextField(15);
         failSearch = new JLabel();
         searchButton = new JButton("Search");
-        backButton = new JButton("Back");
 
         welcomeLabel.setBounds(125,10,150,30);
         welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -32,13 +30,11 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         searchLabel.setHorizontalAlignment(SwingConstants.CENTER);
         searchField.setBounds(125, 90, 150, 30);
         searchButton.setBounds(125, 130, 150, 30);
-        backButton.setBounds(125, 200, 150, 30);
 
         failSearch.setForeground(Color.red);
         failSearch.setBounds(125, 155, 150, 30);
         failSearch.setHorizontalAlignment(SwingConstants.CENTER);
         searchButton.addActionListener(this);
-        backButton.addActionListener(this);
 
         container = getContentPane();
         container.setLayout(null);
@@ -46,7 +42,6 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         container.add(searchLabel);
         container.add(searchField);
         container.add(searchButton);
-        container.add(backButton);
         container.add(failSearch);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -56,22 +51,16 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         //if movie exists in database:
-        if(e.getSource() == searchButton){
-            if(!database.getSpecificMovie(searchField.getText()).isEmpty()){
-             SelectShowtimeGUI showtimeGUI = new SelectShowtimeGUI(searchField.getText(), acc);
-                showtimeGUI.setBounds(10, 10, 400, 400);
-                showtimeGUI.setVisible(true);
-                dispose();
-            }else{
-                failSearch.setText("No movie was found");
-            
-            }
-
-        }else if(e.getSource() == backButton){
-            LoginGUI login = new LoginGUI();
-            login.setBounds(10, 10, 400, 400);
-            login.setVisible(true);
+        
+        if(database.getSpecificMovie(searchField.getText())){
+            SelectShowtimeGUI showtimeGUI = new SelectShowtimeGUI(searchField.getText(), acc);
+            showtimeGUI.setBounds(10, 10, 400, 400);
+            showtimeGUI.setVisible(true);
+            showtimeGUI.setResizable(false);
             dispose();
+        }else{
+            failSearch.setText("No movie was found");
+            
         }
     }
 }
