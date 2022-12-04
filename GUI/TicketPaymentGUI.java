a3a7503b498cd84cc53cadf895aa6cf6f65da4dc
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2022-12-04 15:06:11 -0700

    Update TicketPaymentGUI.java

--- GUI/TicketPaymentGUI.java
+++ GUI/TicketPaymentGUI.java
@@ -1,4 +1,9 @@
+package GUI;
+
 import javax.swing.*;
+
+import SendEmails.SendEmail;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -13,9 +18,11 @@ public class TicketPaymentGUI extends JFrame implements ActionListener {
     private String seat = "Seat Num";
     private JButton confirmButton;
     private Container container;
+    private String email;
 
     //should be given a size of 400, 250
-    public TicketPaymentGUI() {
+    public TicketPaymentGUI(String email) {
+        this.email= email;
         titleLabel = new JLabel("Confirm Ticket Purchase");
         info = new JLabel(movie + " in " + seat + " at " + theatre + " on " + showtime);
         confirmButton = new JButton("Confirm");
@@ -37,12 +44,12 @@ public class TicketPaymentGUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        SendEmail reciept = new SendEmail(this.email, "Ticket Reciept", "please enjoy "+ this.movie +" at "+this.showtime);
+        dispose();
     }
 
     public static void main(String[] args) {
-        TicketPaymentGUI frame = new TicketPaymentGUI();
+        TicketPaymentGUI frame = new TicketPaymentGUI("jwmclean2002@gmail.com");
         frame.setTitle("Payment");
         frame.setVisible(true);
         frame.setBounds(10,10, 400,250);
