77f058f4cfdeb95fbaa7fe5812ccb6fd0eebf938
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-03 23:35:07 -0700

    Add files via upload

---
+++ MainRunner.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+import GUI.LoginGUI;
+
+public class MainRunner {
+    public static void main(String[] args){
+        LoginGUI frame = new LoginGUI();
+        frame.setTitle("Login");
+        frame.setVisible(true);
+        frame.setBounds(10,10, 400,400);
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.setResizable(false);
+    }
+}
