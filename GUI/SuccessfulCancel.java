efe2269cc5287d798912b7cb8372d7b0eb3d0b5f
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-04 02:09:26 -0700

    Add files via upload

---
+++ GUI/SuccessfulCancel.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SuccessfulCancel extends JFrame{
+    private Container container;
+    private JLabel usernameLabel;
+    
+    public SuccessfulCancel() {
+        usernameLabel = new JLabel("The ticket has been refunded");
+        usernameLabel.setBounds(20,50,400,30);
+        container = getContentPane();
+        container.setLayout(null);
+        container.add(usernameLabel);
+    }
+    
+}
