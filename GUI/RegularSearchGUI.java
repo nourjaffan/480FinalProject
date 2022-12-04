6a4970007b1c875cc5a34c47e964e84c724b62ec
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-04 02:30:09 -0700

    Update RegularSearchGUI.java

--- GUI/RegularSearchGUI.java
+++ GUI/RegularSearchGUI.java
@@ -13,6 +13,7 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
     private JLabel searchLabel;
     private JTextField searchField;
     private JButton searchButton;
+    private JLabel failSearch;
     private Container container;
     private Account acc;
     private DatabaseSingleton database = DatabaseSingleton.getOnlyInstance();
@@ -20,6 +21,7 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         welcomeLabel = new JLabel("Welcome");
         searchLabel = new JLabel("Search for Movies");
         searchField = new JTextField(15);
+        failSearch = new JLabel();
         searchButton = new JButton("Search");
 
         welcomeLabel.setBounds(125,10,150,30);
@@ -29,6 +31,9 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         searchField.setBounds(125, 90, 150, 30);
         searchButton.setBounds(125, 130, 150, 30);
 
+        failSearch.setForeground(Color.red);
+        failSearch.setBounds(125, 155, 150, 30);
+        failSearch.setHorizontalAlignment(SwingConstants.CENTER);
         searchButton.addActionListener(this);
 
         container = getContentPane();
@@ -37,7 +42,9 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
         container.add(searchLabel);
         container.add(searchField);
         container.add(searchButton);
+        container.add(failSearch);
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.acc = acc;
     }
 
@@ -49,6 +56,10 @@ public class RegularSearchGUI extends JFrame implements ActionListener {
             SelectShowtimeGUI showtimeGUI = new SelectShowtimeGUI(searchField.getText(), acc);
             showtimeGUI.setBounds(10, 10, 400, 400);
             showtimeGUI.setVisible(true);
+            dispose();
+        }else{
+            failSearch.setText("No movie was found");
+            
         }
     }
 }
