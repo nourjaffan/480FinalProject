16ffe50fc9d058d91808f828ca5d89f485761581
Abdul Moeiz <101293172+AbdulM02@users.noreply.github.com>
2022-12-04 07:21:14 -0700

    Add files via upload

--- GUI/SeatingGUI.java
+++ GUI/SeatingGUI.java
@@ -1,9 +1,4 @@
-package GUI;
-
 import javax.swing.*;
-
-import AccountWork.Account;
-
 import java.awt.*;
 import java.awt.event.*;
 
@@ -13,20 +8,30 @@ public class SeatingGUI extends JFrame implements ActionListener {
     private JButton confirmButton;
     private Container container;
     private JButton[] buttons = new JButton[40];
-    private String selected;
-    private Account acc;
-    public SeatingGUI(String title, String showtime, Account acc) {
+    private int selected = -1;
+    private Color defaultColor;
+
+    public SeatingGUI() {
         titleLabel = new JLabel("Select Seat");
         titleLabel.setBounds(0,10,650,30);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        confirmButton = new JButton("Confirm");
+        confirmButton.setBounds(250, 300, 150, 30);
+        confirmButton.addActionListener(this);
+
+        defaultColor = confirmButton.getBackground();
 
         container = getContentPane();
         container.setLayout(null);
         container.add(titleLabel);
+        container.add(confirmButton);
 
         int x = 30;
         int y = 30;
         for(int i = 0; i < 40; i++) {
+            
+            //NEED CHECK FOR SELECTED SEAT AVAILABLE
+
             JButton seatButton = new JButton("S " + (i + 1));
             buttons[i] = seatButton;
             seatButton.addActionListener(this);
@@ -47,19 +52,31 @@ public class SeatingGUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        for(JButton b : buttons) {
-            if(b == e.getSource()) {
-                b.setBackground(Color.BLUE);
-                JLabel info = new JLabel(b.getText());
-                info.setBounds(0, 300, 150, 30);
-                container.add(info);
-                selected = b.getText();
-                System.out.println(selected);
-                break;
+        if(e.getSource() == confirmButton) {
+            if(selected == -1) {
+                //do nothing
+            } else {    //NEED CHECK FOR REGISTERED USER AND WHETHER SELECTED SEAT IS AVAILABLE
+                System.out.println("here");
+                RegularTicketPaymentGUI frame = new RegularTicketPaymentGUI();
+                frame.setTitle("Payment");
+                frame.setVisible(true);
+                frame.setBounds(10,10, 400,500);
+            }
+        }
+        else {
+            for(JButton b : buttons) {
+                if(b.getBackground() == Color.GREEN) {
+                    b.setBackground(defaultColor);
+                }
+                if(b == e.getSource()) {
+                    b.setBackground(Color.GREEN);
+                    selected = Integer.parseInt(b.getText().substring(2));
+                    System.out.println(selected);
+                }
             }
         }
     }
-/*
+
     public static void main(String[] args) {
         SeatingGUI frame = new SeatingGUI();
         frame.setTitle("Login");
@@ -68,5 +85,4 @@ public class SeatingGUI extends JFrame implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
     }
-    */
 }
