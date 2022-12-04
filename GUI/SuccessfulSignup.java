c3f91a0ddb83808161c417434369967a00d32f47
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2022-12-04 13:31:46 -0700

    Update SuccessfulSignup.java

--- GUI/SuccessfulSignup.java
+++ GUI/SuccessfulSignup.java
@@ -4,16 +4,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SuccessfulSignup extends JFrame{
+public class SuccessfulSignup extends JFrame implements ActionListener{
     private Container container;
+    private JButton Button;
     private JLabel usernameLabel;
     
     public SuccessfulSignup() {
         usernameLabel = new JLabel("You have been registered and are paying $20 annually");
         usernameLabel.setBounds(20,50,400,30);
+        Button = new JButton("continue");
+        Button.addActionListener(this);
+        Button.setBounds(100,200,150,30);
         container = getContentPane();
         container.setLayout(null);
         container.add(usernameLabel);
+        container.add(Button);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == Button){
+            LoginGUI login = new LoginGUI();
+            login.setBounds(10, 10, 400, 400);
+            login.setVisible(true);
+            dispose();
+        }
+        
     }
     
 }
