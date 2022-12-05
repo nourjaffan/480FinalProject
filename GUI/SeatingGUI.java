beb28af379b44cb7142204cdcd2e1da6f8b7331e
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2022-12-04 17:05:49 -0700

    Update SeatingGUI.java

--- GUI/SeatingGUI.java
+++ GUI/SeatingGUI.java
@@ -20,9 +20,15 @@ public class SeatingGUI extends JFrame implements ActionListener {
     private String title;
     private String showTime;
     private Account acc;
+    private int day;
+    private int month;
+    private int year;
     private DatabaseSingleton database = DatabaseSingleton.getOnlyInstance();
 
     public SeatingGUI(String title, String showTime, Account acc, int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
         titleLabel = new JLabel("Select Seat");
         titleLabel.setBounds(0,10,650,30);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -78,14 +84,14 @@ public class SeatingGUI extends JFrame implements ActionListener {
                 //do nothing
             } else {    //NEED CHECK FOR REGISTERED USER AND WHETHER SELECTED SEAT IS AVAILABLE
                 if(acc.getReg()){
-                    TicketPaymentGUI frame = new TicketPaymentGUI(acc, selected, title, showTime);
+                    TicketPaymentGUI frame = new TicketPaymentGUI(acc, selected, title, showTime, this.day, this.month, this.year);
                     frame.setTitle("Payment");
                     frame.setVisible(true);
                     frame.setBounds(10,10, 400,500);
                     frame.setResizable(false);
                     dispose();
                 }else{
-                    RegularTicketPaymentGUI frame = new RegularTicketPaymentGUI(selected, title, showTime);
+                    RegularTicketPaymentGUI frame = new RegularTicketPaymentGUI(selected, title, showTime, this.day, this.month, this.year);
                     frame.setTitle("Payment");
                     frame.setVisible(true);
                     frame.setBounds(10,10, 400,500);
