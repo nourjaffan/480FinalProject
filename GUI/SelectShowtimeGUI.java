ceb3cd90b7a7f7cc7e47e2fdf68760db25ae5648
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-04 17:23:19 -0700

    Update SelectShowtimeGUI.java

--- GUI/SelectShowtimeGUI.java
+++ GUI/SelectShowtimeGUI.java
@@ -19,8 +19,8 @@ public class SelectShowtimeGUI extends JFrame implements ActionListener {
     private String movieName;
     private DatabaseSingleton database = DatabaseSingleton.getOnlyInstance();
     private Account acc;
-
-    public SelectShowtimeGUI(String name, Account acc) {
+    private boolean announced;
+    public SelectShowtimeGUI(String name, Account acc, boolean announced) {
         movieName = name;
         String availableTimes[] = database.getAllShowTimes(movieName).split("\n");
         titleLabel = new JLabel("Select Showtime");
@@ -51,19 +51,38 @@ public class SelectShowtimeGUI extends JFrame implements ActionListener {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.acc = acc;
+        this.announced = announced;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String[] tmp = choices.getSelectedItem().toString().split("/");
-
-        SeatingGUI frame = new SeatingGUI(tmp[3], tmp[4], acc, Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]), Integer.parseInt(tmp[2]));
-        frame.setTitle("Seating");
-        frame.setVisible(true);
-        frame.setBounds(10,10, 680,400);
-        frame.setResizable(false);
-        dispose();
-        // TODO Auto-generated method stub
+        int counter = 0;
+        if(!announced){
+            for(int i = 1; i <= 40; i++){
+                if(!database.getSpecificSeat(movieName, i, tmp[3], Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]), Integer.parseInt(tmp[2]))){
+                    counter++;
+                }
+            }
+            if(counter >= 4){
+                JOptionPane.showMessageDialog(null, "No more reserved seats remaining");
+            }else{
+                SeatingGUI frame = new SeatingGUI(tmp[4], tmp[3], acc, Integer.parseInt(tmp[0]),Integer.parseInt(tmp[1]),Integer.parseInt(tmp[2]));
+                frame.setTitle("Seating");
+                frame.setVisible(true);
+                frame.setBounds(10,10, 680,400);
+                frame.setResizable(false);
+                dispose();
+            }
+        }else{
+            SeatingGUI frame = new SeatingGUI(tmp[4], tmp[3], acc, Integer.parseInt(tmp[0]),Integer.parseInt(tmp[1]),Integer.parseInt(tmp[2]));
+            frame.setTitle("Seating");
+            frame.setVisible(true);
+            frame.setBounds(10,10, 680,400);
+            frame.setResizable(false);
+            dispose();
+        }
+        
         
     }
 }
