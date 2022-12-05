1672920c0700d742c691ca73f06bd1348908c5c7
JackWMclean <102626729+JackWMclean@users.noreply.github.com>
2022-12-04 17:06:09 -0700

    Update TicketPaymentGUI.java

--- GUI/TicketPaymentGUI.java
+++ GUI/TicketPaymentGUI.java
@@ -3,6 +3,8 @@ package GUI;
 import javax.swing.*;
 
 import SendEmails.SendEmail;
+import AccountWork.Account;
+import Database.DatabaseSingleton;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -12,19 +14,29 @@ public class TicketPaymentGUI extends JFrame implements ActionListener {
     private JLabel titleLabel;
     private JLabel info;
 
-    private String movie = "Movie";
-    private String theatre = "Theatre";
-    private String showtime = "Showtime";
-    private String seat = "Seat Num";
+    private String movie;
+    private String showtime;
+    private int seat;
     private JButton confirmButton;
     private Container container;
-    private String email;
+    private Account acc;
+
+    private int day;
+    private int month;
+    private int year;
+    private static DatabaseSingleton access = DatabaseSingleton.getOnlyInstance();
 
     //should be given a size of 400, 250
-    public TicketPaymentGUI(String email) {
-        this.email= email;
+    public TicketPaymentGUI(Account acc, int seatNum, String title, String showTime, int day, int month , int year) {
+        this.acc = acc;
+        this.movie = title;
+        this.showtime = showTime;
+        this.seat = seatNum;
+        this.day = day;
+        this.month = month;
+        this.year = year;
         titleLabel = new JLabel("Confirm Ticket Purchase");
-        info = new JLabel(movie + " in " + seat + " at " + theatre + " on " + showtime);
+        info = new JLabel("Watching " + title + " in seat number " + seatNum + " at " + showtime);
         confirmButton = new JButton("Confirm");
         confirmButton.addActionListener(this);
 
@@ -44,14 +56,16 @@ public class TicketPaymentGUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        SendEmail reciept = new SendEmail(this.email, "Ticket Reciept", "please enjoy "+ this.movie +" at "+this.showtime);
+        access.addNewTicket(this.seat, this.showtime, this.movie, 15.0, this.day, this.month, this.year, this.acc.getEmail(), 1);
+        SendEmail reciept = new SendEmail(this.acc.getEmail(), "Ticket Reciept", "Ticket cost = $15 \n\n please enjoy "+ this.movie +" at "+this.showtime+ " in seat " + this.seat);
         dispose();
     }
-
+/* 
     public static void main(String[] args) {
         TicketPaymentGUI frame = new TicketPaymentGUI("jwmclean2002@gmail.com");
         frame.setTitle("Payment");
         frame.setVisible(true);
         frame.setBounds(10,10, 400,250);
     }
+    */
 }
