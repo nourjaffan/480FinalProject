30974cf4588415abe9d34a476a9883c03e52d02b
Xian Wei Low <51501780+xianlow@users.noreply.github.com>
2022-12-05 19:01:31 -0700

    Update AdminNewsGUI.java

--- GUI/AdminNewsGUI.java
+++ GUI/AdminNewsGUI.java
@@ -22,6 +22,7 @@ public class AdminNewsGUI extends JFrame implements ActionListener {
     private JButton confirmButton;
     private Container container;
     private DatabaseSingleton database = DatabaseSingleton.getOnlyInstance();
+
     public AdminNewsGUI() {
         newsLabel = new JLabel("Add News");
         titleLabel = new JLabel("Title");
@@ -37,17 +38,17 @@ public class AdminNewsGUI extends JFrame implements ActionListener {
         confirmButton = new JButton("Confirm");
         confirmButton.addActionListener(this);
 
-        newsLabel.setBounds(125,10,150,30);
+        newsLabel.setBounds(125, 10, 150, 30);
         newsLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        titleLabel.setBounds(50,60,100,30);
-        dayLabel.setBounds(50,110,100,30);
-        monthLabel.setBounds(50,160,100,30);
-        yearLabel.setBounds(50,210,100,30);
-        titleField.setBounds(150,60,100,30);
-        dayField.setBounds(150,110,100,30);
-        monthField.setBounds(150,160,100,30);
-        yearField.setBounds(150,210,100,30);
-        confirmButton.setBounds(125,260,150,50);
+        titleLabel.setBounds(50, 60, 100, 30);
+        dayLabel.setBounds(50, 110, 100, 30);
+        monthLabel.setBounds(50, 160, 100, 30);
+        yearLabel.setBounds(50, 210, 100, 30);
+        titleField.setBounds(150, 60, 100, 30);
+        dayField.setBounds(150, 110, 100, 30);
+        monthField.setBounds(150, 160, 100, 30);
+        yearField.setBounds(150, 210, 100, 30);
+        confirmButton.setBounds(125, 260, 150, 50);
 
         container = getContentPane();
         container.setLayout(null);
@@ -65,9 +66,18 @@ public class AdminNewsGUI extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        database.addNewNews(titleField.getText(), Integer.parseInt(dayField.getText()), Integer.parseInt(monthField.getText()), Integer.parseInt(yearField.getText()));
-        JOptionPane.showMessageDialog(null, "The news has been added");
-        // TODO Auto-generated method stub
-        
+        if (titleField.getText().isEmpty() || dayField.getText().isEmpty() || monthField.getText().isEmpty()
+                || yearField.getText().isEmpty()) {
+
+            JOptionPane.showMessageDialog(null, "Please enter all info");
+
+        } else {
+            database.addNewNews(titleField.getText(), Integer.parseInt(dayField.getText()),
+                    Integer.parseInt(monthField.getText()), Integer.parseInt(yearField.getText()));
+                    
+            JOptionPane.showMessageDialog(null, "The news has been added");
+            // TODO Auto-generated method stub
+
+        }
     }
 }
